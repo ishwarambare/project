3a36fa45720a3bc97865378fd064e32f591814bd
ishwar ambare <ishuambare@gmail.com>
2020-12-02 12:09:59 +0530

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(Base):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+    short_description = models.CharField(blank=True, null=True, max_length=2000)
+
+
+class Tag(Base):
+    name = models.CharField(max_length=100)
+
+
+class Post(Base):
+    name = models.CharField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='post')
+    slug = models.CharField(max_length=500, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    post = models.ManyToManyField(Tag)
+    likes = models.PositiveIntegerField(default=0)
+    user_likes = models.ManyToManyField(User)
+
+
+class Comment(Base):
+    comment = models.CharField(max_length=500)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
